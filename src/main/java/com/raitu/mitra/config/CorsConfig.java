9c6de4d96f24a3e27c43f9a21c5dc2c5439d9d03
hari <panthaganihari1@gmail.com>
2026-03-05 22:31:58 +0530

    Add Netlify URL to CORS config

--- src/main/java/com/raitu/mitra/config/CorsConfig.java
+++ src/main/java/com/raitu/mitra/config/CorsConfig.java
@@ -13,10 +13,10 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://raitu-maitra.netlify.app");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
